a2c9fc70dc507923794c82f03485606fe599e226
fmakareev <fmakareev@gmail.com>
2024-08-18 23:43:29 -0300

    pass stage

--- app/DNSHeaderEncoder.ts
+++ app/DNSHeaderEncoder.ts
@@ -1,25 +1,31 @@
 import {DNSPacket} from "./DNSPacket.type";
 
-
 export const dnsPacketEncode = (packet: DNSPacket) => {
     const buffer = Buffer.alloc(12);
 
-
     buffer.writeUInt16BE(packet.packetIdentifier, 0);
 
-    buffer.writeUInt8(packet.qr, 2);
-    buffer.writeUInt8(packet.opcode, 3);
-    buffer.writeUInt8(packet.authoritativeAnswer, 4);
-    buffer.writeUInt8(packet.truncation, 5);
-    buffer.writeUInt8(packet.recursionDesired, 6);
-    buffer.writeUInt8(packet.recursionAvailable, 7);
-    buffer.writeUInt8(packet.reserved, 8);
-    buffer.writeUInt8(packet.responseCode, 9);
+    let flags = 0;
+    flags |= (packet.qr & 0x01) << 15;
+    flags |= (packet.opcode & 0x0F) << 11;
+    flags |= (packet.authoritativeAnswer & 0x01) << 10;
+    flags |= (packet.truncation & 0x01) << 9;
+    flags |= (packet.recursionDesired & 0x01) << 8;
+    flags |= (packet.recursionAvailable & 0x01) << 7;
+    flags |= (packet.reserved & 0x07) << 4;
+    flags |= (packet.responseCode & 0x0F);
 
-    buffer.writeUInt8(packet.questionCount, 10);
-    buffer.writeUInt8(packet.answerRecordCount, 12);
-    buffer.writeUInt8(packet.authorityRecordCount, 14);
-    buffer.writeUInt8(packet.additionalRecordCount, 16);
+    buffer.writeUInt16BE(flags, 2);
+
+    buffer.writeUInt16BE(packet.questionCount, 4);
+
+
+    buffer.writeUInt16BE(packet.answerRecordCount, 6);
+
+
+    buffer.writeUInt16BE(packet.authorityRecordCount, 8);
+
+    buffer.writeUInt16BE(packet.additionalRecordCount, 10);
 
     return buffer;
 }
